5b6c02ac5d74c4d337bb3374327f5ffb37d06e95
Yalchan <shveppess@abv.bg>
2019-12-10 17:28:59 +0200

    Last Commit

---
+++ Last_Commit/src/model/Admin.java
@@ -0,0 +1,19 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="admins")
+public class Admin extends User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id_admin")
+    private Integer idAdmin;
+    public Admin(){
+
+    }
+    public Admin(String userName,String password,String firstName,String lastName){
+        super(userName,password,firstName,lastName);
+    }
+
+}
---
+++ Last_Commit/src/repositories/TenantRepository.java
@@ -0,0 +1,34 @@
+package repositories;
+
+import model.Tenant;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class TenantRepository {
+
+    public void save(Tenant tenant){
+        SessionFactory factory;
+        factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+        session.save(tenant);
+        t.commit();
+        session.close();
+    }
+    public List<Tenant> tenantList(){
+        SessionFactory factory;
+        factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+        Query query=session.createQuery("from Tenant");
+        List<Tenant> tenantList=query.list();
+        t.commit();
+        session.close();
+        return tenantList;
+    }
+ }
